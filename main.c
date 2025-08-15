cecb72d2eacf0ce2ddb7dfdab02af36805d0f3a3
Gusti199410 <87740299+Gusti199410@users.noreply.github.com>
2025-08-15 13:45:49 -0300

    nuevaspractica

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 int main()
 {
     char destino[30];
+
     char origen[30]="-42a";
-   // size_t ce=sizeof(origen)/sizeof(origen[0]);
+
     printf("%d",mi_funcio_Atoi(origen));
+
     return 0;
 }
